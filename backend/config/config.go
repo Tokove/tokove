79e47ccbed79c5d48286da152487778379fd7b53
zzh <jlzhouzihan@163.com>
2025-10-24 20:08:21 +0800

    增加配置信息和初始化函数

--- backend/config/config.go
+++ backend/config/config.go
@@ -1,1 +1,43 @@
 package config
+
+import (
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	App struct {
+		Name string
+		Port string
+	}
+	Database struct {
+		Dsn          string
+		MaxIdleConns uint
+		MaxOpenConns uint
+	}
+	Redis struct {
+		Addr     string
+		DB       uint
+		Password string
+	}
+}
+
+var AppConfig *Config
+
+func InitConfig() {
+	// 设置要读取的配置文件信息
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath("/config")
+
+	// 读取配置文件
+	if err := viper.ReadInConfig(); err != nil {
+		panic("读取配置文件失败: " + err.Error())
+	}
+
+	// 解析
+	AppConfig = &Config{}
+	if err := viper.Unmarshal(AppConfig); err != nil {
+		panic("解析配置文件失败: " + err.Error())
+	}
+
+}
